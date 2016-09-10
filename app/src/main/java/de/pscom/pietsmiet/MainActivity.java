6880468db58c38acc3f84c484e352cfa53fb9c85
Thomas Lu <thomasvanhalen01@gmail.com>
2016-09-10 17:57:36 +0200

    parse uploadplan with rxjava and add retrolambda

--- app/src/main/java/de/pscom/pietsmiet/MainActivity.java
+++ app/src/main/java/de/pscom/pietsmiet/MainActivity.java
@@ -1,15 +1,27 @@
 package de.pscom.pietsmiet;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSItem;
+import org.mcsoxford.rss.RSSReader;
+
+import java.util.List;
+
+import rx.Observable;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +39,32 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        Subscription mSubscription = Observable.defer(() -> Observable.just(loadRss()))
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(results -> {
+                    Observable.from(results)
+                            .map(srcObj -> srcObj.getLink().toString())
+                            .subscribe(result -> Log.v("Tag", result));
+                }, e -> Log.v("Error", e.toString()));
+
+        Log.v("tag", "hasdsi");
+    }
+
+    List<RSSItem> loadRss() {
+
+        RSSReader reader = new RSSReader();
+        String uri = "http://pietsmiet.de/news?format=feed&type=rss";
+        RSSFeed feed;
+        try {
+            feed = reader.load(uri);
+            return feed.getItems();
+        } catch (Exception rssException) {
+            Log.v("Tag", rssException.toString());
+        }
+        return null;
+
     }
 
     @Override

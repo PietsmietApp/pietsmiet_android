738def453577fcae3855c2d2a0bdc9cea3f7466c
Thomas Lu <l3d00m@users.noreply.github.com>
2016-09-12 20:06:06 +0200

    Fetch latest uploadplan & parse HTML (#6)
    * parse uploadplan with rxjava and add retrolambda
    
    * externalize rss code and simplify observable
    
    * parse website for getting the uploadplan
    
    * add pietcast, rename class
    
    * use log class
    
    * avoid null links, add a maximum of html to parse

--- app/src/main/java/de/pscom/pietsmiet/MainActivity.java
+++ app/src/main/java/de/pscom/pietsmiet/MainActivity.java
@@ -1,21 +1,24 @@
 package de.pscom.pietsmiet;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
 import java.util.ArrayList;
 
 import de.pscom.pietsmiet.adapters.CardItem;
 import de.pscom.pietsmiet.adapters.CardViewAdapter;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

98093f34b7a81972a3534748835dc7d89ab8bdc9
l3d00m <substantialimpulse@protonmail.com>
2016-10-13 22:21:05 +0200

    Design changes and new presenters (#9)
    * add connection from backend to frontend with a presenter modell
    
    * add fb presenter, sort all cards by time desc
    
    * replace twitter names through manual names
    
    * optimize code, add a drawable fetcher class
    
    * revert to ints instead of enum
    
    * bug fixes, add javadoc

--- app/src/main/java/de/pscom/pietsmiet/MainActivity.java
+++ app/src/main/java/de/pscom/pietsmiet/MainActivity.java
@@ -10,21 +10,30 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
 
 import de.pscom.pietsmiet.adapters.CardItem;
-import de.pscom.pietsmiet.adapters.SocialCardItem;
-import de.pscom.pietsmiet.adapters.VideoCardItem;
 import de.pscom.pietsmiet.adapters.CardViewAdapter;
-import de.pscom.pietsmiet.util.FacebookHelper;
+import de.pscom.pietsmiet.backend.FacebookPresenter;
+import de.pscom.pietsmiet.backend.RssPresenter;
+import de.pscom.pietsmiet.backend.TwitterPresenter;
+import de.pscom.pietsmiet.util.DrawableFetcher;
 
-import static de.pscom.pietsmiet.adapters.CardItem.CardItemType.TYPE_PIETCAST;
-import static de.pscom.pietsmiet.adapters.CardItem.CardItemType.TYPE_TWITTER;
-import static de.pscom.pietsmiet.adapters.CardItem.CardItemType.TYPE_UPLOAD_PLAN;
-import static de.pscom.pietsmiet.adapters.CardItem.CardItemType.TYPE_VIDEO;
+import static de.pscom.pietsmiet.util.CardTypes.TYPE_PIETCAST;
+import static de.pscom.pietsmiet.util.CardTypes.TYPE_TWITTER;
+import static de.pscom.pietsmiet.util.CardTypes.TYPE_UPLOAD_PLAN;
+import static de.pscom.pietsmiet.util.CardTypes.TYPE_VIDEO;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private CardViewAdapter adapter;
+    private List<CardItem> cardItems = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,40 +54,61 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         //Only for testing
         new Thread(() -> {
             ArrayList<CardItem> cardItems = new ArrayList<>();
-            cardItems.add(new VideoCardItem("PietCast #79 - Krötenwehr",
+            cardItems.add(new CardItem("PietCast #79 - Krötenwehr",
                     "Der erste Podcast nach unserer Pause und es gab super viel zu bereden. Wir haben über unseren Urlaub gesprochen. Darüber wie wir mit Hate und Flame umgehen. Warum Produktplatzierungen existieren und warum wir sie machen. Warum Maschinenbau ein geiler Studiengang ist und zu guter Letzt welche 5 Personen auf einer Non-Cheat Liste stehen würden. Ihr wisst nicht was das ist!",
-                    "Montag, 25. Dezember",
-                    "http://www.pietcast.de/pietcast/wp-content/uploads/2016/09/thumbnail-672x372.png",
+                    new Date(),
+                    DrawableFetcher.getDrawableFromUrl("http://www.pietcast.de/pietcast/wp-content/uploads/2016/09/thumbnail-672x372.png"),
                     TYPE_PIETCAST));
-            cardItems.add(new VideoCardItem("HOCKENHEIMRING-TRAINING 2/2 \uD83C\uDFAE F1 2016 #3",
-                    "HOCKENHEIMRING-TRAINING 2/2 \uD83C\uDFAE F1 2016 #3\nDauer: 30 Minuten",
-                    "Montag, 25. Dezember um 13.00 Uhr",
-                    "http://img.youtube.com/vi/0g2knLku2MM/hqdefault.jpg",
+            cardItems.add(new CardItem("HOCKENHEIMRING-TRAINING 2/2 \uD83C\uDFAE F1 2016 #3",
+                    "HOCKENHEIMRING-TRAINING 2/2 \uD83C\uDFAE F1 2016 #3",
+                    new Date(),
+                    DrawableFetcher.getDrawableFromUrl("http://img.youtube.com/vi/0g2knLku2MM/hqdefault.jpg"),
                     TYPE_VIDEO));
-            cardItems.add(new SocialCardItem("Uploadplan am 11.09.2016",
-                    "14:00 Uhr: Osiris\n15:00 Uhr: Titan 3\n16:00 Uhr: Gears of War 4\n18:00 Uhr: Mario Kart 8",
+            cardItems.add(new CardItem("Uploadplan am 11.09.2016",
+                    "14:00 Uhr: Osiris<br>15:00 Uhr: Titan 3<br>16:00 Uhr: Gears of War 4<br>18:00 Uhr: Mario Kart 8",
+                    new Date(),
                     TYPE_UPLOAD_PLAN));
-            cardItems.add(new SocialCardItem("Dr.Jay auf Twitter",
-                    "Wow ist das Bitter für #Hamilton Sorry for that :-( @LewisHamilton #MalaysiaGP",
-                    "Montag, 25. Dezember um 14:69 Uhr",
+            cardItems.add(new CardItem("Dr.Jay auf Twitter",
+                    "Wow ist das Bitter für #Hamilton Sorry for that :-( @LewisHamilton #MalaysiaGP http://pietsmiet.de",
+                    new Date(),
                     TYPE_TWITTER));
 
-            runOnUiThread(() -> showCardViewItems(cardItems));
+            runOnUiThread(() -> {
+                for (CardItem cardItem : cardItems) addNewCard(cardItem);
+            });
         }).start();
+        setupRecyclerView();
+
+        new TwitterPresenter().onTakeView(this);
+        new RssPresenter().onTakeView(this);
+        new FacebookPresenter().onTakeView(this);
     }
 
-    public void showCardViewItems(ArrayList<CardItem> cardItems) {
+    public void setupRecyclerView() {
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.cardList);
-
-        CardViewAdapter adapter = new CardViewAdapter(cardItems, this);
+        adapter = new CardViewAdapter(cardItems, this);
         LinearLayoutManager llm = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(llm);
         recyclerView.setAdapter(adapter);
     }
 
+    public void addNewCard(CardItem item) {
+        cardItems.add(item);
+        Collections.sort(cardItems);
+        if (adapter != null) adapter.notifyDataSetChanged();
+    }
+
+    public void showError(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+    }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         return false;
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
     }
 }
--- app/src/main/java/de/pscom/pietsmiet/adapters/CardItem.java
+++ app/src/main/java/de/pscom/pietsmiet/adapters/CardItem.java
@@ -2,41 +2,85 @@ package de.pscom.pietsmiet.adapters;
 
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
-import java.io.InputStream;
-import java.net.URL;
+import java.util.Date;
 
-import de.pscom.pietsmiet.util.PsLog;
+import de.pscom.pietsmiet.util.CardTypes.ItemTypeNoThumbnail;
+import de.pscom.pietsmiet.util.CardTypes.ItemTypeThumbnail;
 
-public class CardItem {
+import static de.pscom.pietsmiet.util.CardTypes.TYPE_FACEBOOK;
+import static de.pscom.pietsmiet.util.CardTypes.TYPE_PIETCAST;
+import static de.pscom.pietsmiet.util.CardTypes.TYPE_STREAM;
+import static de.pscom.pietsmiet.util.CardTypes.TYPE_TWITTER;
+import static de.pscom.pietsmiet.util.CardTypes.TYPE_UPLOAD_PLAN;
+import static de.pscom.pietsmiet.util.CardTypes.TYPE_VIDEO;
 
+public class CardItem implements Comparable<CardItem> {
     String description;
-    String datetime;
+    Date datetime;
     String title;
-    Drawable preview;
-    CardItemType cardItemType;
+    Drawable thumbnail;
+    int cardItemType;
 
-    CardItem(String title, String description, String datetime, Drawable preview, CardItemType cardItemType) {
+
+    /**
+     * Creates a new card item
+     *
+     * @param title        Title of the card
+     * @param description  Description or message of the card
+     * @param datetime     Time of the card
+     * @param cardItemType Type of the card
+     */
+
+    public CardItem(String title, String description, Date datetime, @ItemTypeNoThumbnail int cardItemType) {
         this.title = title;
         this.description = description;
         this.datetime = datetime;
-        this.preview = preview;
         this.cardItemType = cardItemType;
     }
 
-    public CardItemType getCardItemType() {
+    /**
+     * Creates a new card item with a thumbnail
+     *
+     * @param title        Title of the card
+     * @param description  Description or message of the card
+     * @param datetime     Time of the card
+     * @param cardItemType Type of the card
+     * @param thumbnail    Thumbnail image
+     */
+    public CardItem(String title, String description, Date datetime, Drawable thumbnail, @ItemTypeThumbnail int cardItemType) {
+        this.title = title;
+        this.description = description;
+        this.datetime = datetime;
+        this.thumbnail = thumbnail;
+        this.cardItemType = cardItemType;
+    }
+
+
+    @Nullable
+    public Drawable getThumbnail() {
+        return this.thumbnail;
+    }
+
+    public void setThumbnail(Drawable thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public int getCardItemType() {
         return cardItemType;
     }
 
-    public void setCardItemType(CardItemType type) {
+    public void setCardItemType(int type) {
         this.cardItemType = type;
     }
 
-    public String getDatetime() {
+    public Date getDatetime() {
         return datetime;
     }
 
-    public void setDatetime(String datetime) {
+    public void setDatetime(Date datetime) {
         this.datetime = datetime;
     }
 
@@ -49,7 +93,29 @@ public class CardItem {
     }
 
     public int getBackgroundColor() {
-        return Color.parseColor("#bdbdbd");
+        String hexColor;
+        switch (cardItemType) {
+            case TYPE_VIDEO:
+            case TYPE_STREAM:
+                hexColor = "#ef5350";
+                break;
+            case TYPE_PIETCAST:
+                hexColor = "#5c6bc0";
+                break;
+            case TYPE_FACEBOOK:
+                hexColor = "#ff7043";
+                break;
+            case TYPE_TWITTER:
+                hexColor = "#42a5f5";
+                break;
+            case TYPE_UPLOAD_PLAN:
+                hexColor = "#26a69a";
+                break;
+            default:
+                hexColor = "#bdbdbd";
+                break;
+        }
+        return Color.parseColor(hexColor);
     }
 
     public String getTitle() {
@@ -60,28 +126,19 @@ public class CardItem {
         this.title = title;
     }
 
-    static Drawable getDrawableFromUrl(String url) {
-        try {
-            InputStream is = (InputStream) new URL(url).getContent();
-            return Drawable.createFromStream(is, "src name");
-        } catch (Exception e) {
-            PsLog.w("Couldn't fetch thumbnail: " + e.toString());
-            return null;
-        }
-    }
-
-    public enum CardItemType {
-        TYPE_VIDEO,
-        TYPE_STREAM,
-        TYPE_PIETCAST,
-        TYPE_TWITTER,
-        TYPE_FACEBOOK,
-        TYPE_UPLOAD_PLAN
-    }
 
     boolean isVideoView() {
-        return cardItemType.equals(CardItemType.TYPE_VIDEO)
-                || cardItemType.equals(CardItemType.TYPE_STREAM)
-                || cardItemType.equals(CardItemType.TYPE_PIETCAST);
+        return cardItemType == TYPE_VIDEO
+                || cardItemType == TYPE_STREAM
+                || cardItemType == TYPE_PIETCAST;
     }
+
+    @Override
+    public int compareTo(@NonNull CardItem item) {
+        if (item.getDatetime() == null) return -1;
+        else if (this.getDatetime() == null) return 1;
+        return item.getDatetime().compareTo(this.getDatetime());
+    }
+
+
 }

d3c27a58d32da855ad2c412491383d57283fcb2f
Thomas Lu <thomasvanhalen01@gmail.com>
2016-10-13 22:19:53 +0200

    fix a merge error

--- app/src/main/java/de/pscom/pietsmiet/MainActivity.java
+++ app/src/main/java/de/pscom/pietsmiet/MainActivity.java
@@ -18,8 +18,6 @@ import java.util.Date;
 import java.util.List;
 
 import de.pscom.pietsmiet.adapters.CardItem;
-import de.pscom.pietsmiet.adapters.SocialCardItem;
-import de.pscom.pietsmiet.adapters.VideoCardItem;
 import de.pscom.pietsmiet.adapters.CardViewAdapter;
 import de.pscom.pietsmiet.backend.FacebookPresenter;
 import de.pscom.pietsmiet.backend.RssPresenter;

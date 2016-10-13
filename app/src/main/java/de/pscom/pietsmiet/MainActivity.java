8aba87f3eec9608add2351a415c8296c9ee7c52d
l3d00m <substantialimpulse@protonmail.com>
2016-10-13 22:15:13 +0200

    New card design (#10)
    * Small redesign of card view
    
    * use thumbnail cards instead of full width pictures
    
    * clean the associated code to cardview (this shouldn't have happened in that branch but whatever)

--- app/src/main/java/de/pscom/pietsmiet/MainActivity.java
+++ app/src/main/java/de/pscom/pietsmiet/MainActivity.java
@@ -14,9 +14,15 @@ import android.view.MenuItem;
 import java.util.ArrayList;
 
 import de.pscom.pietsmiet.adapters.CardItem;
+import de.pscom.pietsmiet.adapters.SocialCardItem;
+import de.pscom.pietsmiet.adapters.VideoCardItem;
 import de.pscom.pietsmiet.adapters.CardViewAdapter;
 import de.pscom.pietsmiet.util.FacebookHelper;
 
+import static de.pscom.pietsmiet.adapters.CardItem.CardItemType.TYPE_PIETCAST;
+import static de.pscom.pietsmiet.adapters.CardItem.CardItemType.TYPE_TWITTER;
+import static de.pscom.pietsmiet.adapters.CardItem.CardItemType.TYPE_UPLOAD_PLAN;
+import static de.pscom.pietsmiet.adapters.CardItem.CardItemType.TYPE_VIDEO;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     @Override
@@ -30,7 +36,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.dl_root);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -39,12 +45,23 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         //Only for testing
         new Thread(() -> {
             ArrayList<CardItem> cardItems = new ArrayList<>();
-            cardItems.add(new CardItem("PietCast #79", "Der erste Podcast nach unserer Pause und es gab super viel zu bereden. Wir haben über unseren Urlaub gesprochen. Darüber wie wir mit Hate und Flame umgehen. Warum Produktplatzierungen existieren und warum wir sie machen. Warum Maschinenbau ein geiler Studiengang ist und zu guter Letzt welche 5 Personen auf einer Non-Cheat Liste stehen würden. Ihr wisst nicht was das ist \nDauer: 2:50 Stunden",
-                    "11.09.2016 13:00 Uhr", getResources().getDrawable(R.drawable.ic_music_note_black_24dp), "http://www.pietcast.de/pietcast/wp-content/uploads/2016/09/thumbnail-672x372.png", CardItem.TYPE_PIETCAST));
-            cardItems.add(new CardItem("Neues Video", "HOCKENHEIMRING-TRAINING 2/2 \uD83C\uDFAE F1 2016 #3\nDauer: 30 Minuten",
-                    "11.09.2016 13:00 Uhr", getResources().getDrawable(R.drawable.youtube), "http://img.youtube.com/vi/0g2knLku2MM/hqdefault.jpg", CardItem.TYPE_VIDEO));
-            cardItems.add(new CardItem("Neuer Uploadplan", "14:00 Uhr TTT\n15:00 Uhr TTT\n" + "16:00 Uhr TTT\n18:00 Uhr TTT\n20:00 Uhr TTT",
-                    "12.09.2016 13:00 Uhr", getResources().getDrawable(R.drawable.ic_assignment_black_24dp), CardItem.TYPE_UPLOAD_PLAN));
+            cardItems.add(new VideoCardItem("PietCast #79 - Krötenwehr",
+                    "Der erste Podcast nach unserer Pause und es gab super viel zu bereden. Wir haben über unseren Urlaub gesprochen. Darüber wie wir mit Hate und Flame umgehen. Warum Produktplatzierungen existieren und warum wir sie machen. Warum Maschinenbau ein geiler Studiengang ist und zu guter Letzt welche 5 Personen auf einer Non-Cheat Liste stehen würden. Ihr wisst nicht was das ist!",
+                    "Montag, 25. Dezember",
+                    "http://www.pietcast.de/pietcast/wp-content/uploads/2016/09/thumbnail-672x372.png",
+                    TYPE_PIETCAST));
+            cardItems.add(new VideoCardItem("HOCKENHEIMRING-TRAINING 2/2 \uD83C\uDFAE F1 2016 #3",
+                    "HOCKENHEIMRING-TRAINING 2/2 \uD83C\uDFAE F1 2016 #3\nDauer: 30 Minuten",
+                    "Montag, 25. Dezember um 13.00 Uhr",
+                    "http://img.youtube.com/vi/0g2knLku2MM/hqdefault.jpg",
+                    TYPE_VIDEO));
+            cardItems.add(new SocialCardItem("Uploadplan am 11.09.2016",
+                    "14:00 Uhr: Osiris\n15:00 Uhr: Titan 3\n16:00 Uhr: Gears of War 4\n18:00 Uhr: Mario Kart 8",
+                    TYPE_UPLOAD_PLAN));
+            cardItems.add(new SocialCardItem("Dr.Jay auf Twitter",
+                    "Wow ist das Bitter für #Hamilton Sorry for that :-( @LewisHamilton #MalaysiaGP",
+                    "Montag, 25. Dezember um 14:69 Uhr",
+                    TYPE_TWITTER));
 
             runOnUiThread(() -> showCardViewItems(cardItems));
         }).start();

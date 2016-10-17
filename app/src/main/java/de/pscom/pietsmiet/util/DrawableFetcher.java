f1f76f2d07970adf67d421c4b24884d6f7648502
l3d00m <substantialimpulse@protonmail.com>
2016-10-17 10:37:28 +0200

    Make nav drawer functional and load thumbnails (#11)
    * add drawer functionality with help of a new cardmanager class
    
    * use a mainpresenter to reduce duplicate code
    
    * fetch thumbnails of tweets, posts and rss-feeds
    
    * make picture full size on social cards
    
    * add "getDrawableFromRss" method
    
    * add javadoc for easier understanding

--- app/src/main/java/de/pscom/pietsmiet/util/DrawableFetcher.java
+++ app/src/main/java/de/pscom/pietsmiet/util/DrawableFetcher.java
@@ -4,8 +4,12 @@ import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import org.mcsoxford.rss.MediaThumbnail;
+import org.mcsoxford.rss.RSSItem;
+
 import java.io.InputStream;
 import java.net.URL;
+import java.util.List;
 
 import facebook4j.Post;
 import twitter4j.MediaEntity;
@@ -13,6 +17,10 @@ import twitter4j.Status;
 
 public class DrawableFetcher {
 
+    /**
+     * @param post A facebook posting
+     * @return The drawable from the post, if available
+     */
     @Nullable
     public static Drawable getDrawableFromPost(@Nullable Post post) {
         if (post != null) {
@@ -24,6 +32,28 @@ public class DrawableFetcher {
         return null;
     }
 
+    /**
+     * @param rssItem A rss item
+     * @return The drawable from the item, if available
+     */
+    @Nullable
+    public static Drawable getDrawableFromRss(@Nullable RSSItem rssItem) {
+        if (rssItem != null) {
+            List<MediaThumbnail> thumbs = rssItem.getThumbnails();
+            if (thumbs != null
+                    && thumbs.size() > 0
+                    && thumbs.get(0) != null
+                    && thumbs.get(0).getUrl() != null) {
+                return getDrawableFromUrl(thumbs.get(0).getUrl().toString());
+            }
+        }
+        return null;
+    }
+
+    /**
+     * @param status A tweet
+     * @return The drawable from the tweet, if available
+     */
     @Nullable
     public static Drawable getDrawableFromTweet(@Nullable Status status) {
         if (status != null) {
@@ -35,15 +65,19 @@ public class DrawableFetcher {
         return null;
     }
 
+    /**
+     * @param url Url to the image
+     * @return A drawable from the url
+     */
     @Nullable
     public static Drawable getDrawableFromUrl(@NonNull String url) {
         try {
-            PsLog.v("Fetching drawable from URL: " + url);
             InputStream is = (InputStream) new URL(url).getContent();
-            return Drawable.createFromStream(is, "src name");
+            Drawable toReturn = Drawable.createFromStream(is, "src name");
+            if (toReturn.getMinimumHeight() > 0 && toReturn.getMinimumWidth() > 0) return toReturn;
         } catch (Exception e) {
             PsLog.w("Couldn't fetch thumbnail: " + e.toString());
-            return null;
         }
+        return null;
     }
 }

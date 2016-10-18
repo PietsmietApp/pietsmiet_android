4c56d01d4a9ee7d822a6809d217cf003a21c1a7c
l3d00m <substantialimpulse@protonmail.com>
2016-10-18 16:30:32 +0200

    hide secrets in file, fix some bugs after merge

---
+++ app/src/main/java/de/pscom/pietsmiet/util/SecretConstants.java
@@ -0,0 +1,37 @@
+package de.pscom.pietsmiet.util;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class SecretConstants {
+    @Nullable
+    public static String twitterSecret;
+    @Nullable
+    public static String rssUrl;
+    @Nullable
+    public static String facebookToken;
+    @Nullable
+    public static String facebookSecret;
+
+    public SecretConstants(Context context) {
+        try {
+            InputStream input = context.getAssets().open("secrets.properties");
+            Properties properties = new Properties();
+            properties.load(input);
+
+            twitterSecret = properties.getProperty("twitterSecret");
+            rssUrl = properties.getProperty("rssUrl");
+            facebookToken = properties.getProperty("facebookToken");
+            facebookSecret = properties.getProperty("facebookSecret");
+        } catch (IOException e) {
+            e.printStackTrace();
+            PsLog.w("You haven't included a '/app/src/main/assets/secrets.properties' file in your project with the API-Keys and RSS-URLs!\n" +
+                    "=> Twitter, Facebook & Uploadplan won't work");
+        }
+    }
+
+}

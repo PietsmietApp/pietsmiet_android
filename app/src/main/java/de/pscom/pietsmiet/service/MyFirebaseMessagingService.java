0e6d28124e6b02b1d54737cd5ddf1f80bef9877f
Thomas Lu <thomasvanhalen01@gmail.com>
2016-11-29 18:28:07 +0100

    WIP: Update code to new backend code
    * remove unused firebaseinstanceidservice

--- app/src/main/java/de/pscom/pietsmiet/service/MyFirebaseMessagingService.java
+++ app/src/main/java/de/pscom/pietsmiet/service/MyFirebaseMessagingService.java
@@ -30,6 +30,7 @@ import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 import de.pscom.pietsmiet.MainActivity;
+import de.pscom.pietsmiet.R;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
@@ -50,10 +51,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
             Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-        }
-        // Check if message contains a notification payload.
-        if (remoteMessage.getNotification() != null) {
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            sendNotification(remoteMessage.getData().get("title"), remoteMessage.getData().get("message"));
         }
 
         // Also if you intend on generating your own notifications as a result of a received FCM
@@ -65,16 +63,17 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
      *
      * @param messageBody FCM message body received.
      */
-    private void sendNotification(String messageBody) {
+    private void sendNotification(String title, String messageBody) {
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
-                PendingIntent.FLAG_ONE_SHOT);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* todo Request code */, intent,
+                PendingIntent.FLAG_CANCEL_CURRENT);
 
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                //.setSmallIcon(R.drawable.ic_stat_ic_notification)
-                .setContentTitle("FCM Message")
+                .setSmallIcon(R.drawable.pietcast_placeholder) //fixme
+                .setContentTitle(title)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(messageBody))
                 .setContentText(messageBody)
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
@@ -83,6 +82,6 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+        notificationManager.notify(65, notificationBuilder.build());
     }
 }

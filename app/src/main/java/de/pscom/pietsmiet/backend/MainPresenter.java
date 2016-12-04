e097d4adbbe36b53b73da5e5737758b738037fbd
Thomas Lu <l3d00m@users.noreply.github.com>
2016-12-04 17:14:34 +0100

    Use a post builder for creating post objects (#30)

--- app/src/main/java/de/pscom/pietsmiet/backend/MainPresenter.java
+++ app/src/main/java/de/pscom/pietsmiet/backend/MainPresenter.java
@@ -14,7 +14,7 @@ class MainPresenter {
     final MainActivity view;
     @TypeAllPosts
     private final int postType;
-    Post post;
+    Post.PostBuilder postBuilder;
     @SuppressWarnings("CanBeFinal")
     List<Post> posts = new ArrayList<>();
 
--- app/src/main/java/de/pscom/pietsmiet/backend/YoutubePresenter.java
+++ app/src/main/java/de/pscom/pietsmiet/backend/YoutubePresenter.java
@@ -55,22 +55,21 @@ public class YoutubePresenter extends MainPresenter {
                 .flatMapIterable(YoutubeRoot::getItems)
                 .filter(result -> result != null)
                 .doOnNext(item -> {
-                    this.post = new Post();
+                    this.postBuilder = new Post.PostBuilder(VIDEO);
                     String videoID = item.getContentDetails().getVideoId();
                     if (videoID != null && !videoID.isEmpty()) {
-                        post.setUrl("http://www.youtube.com/watch?v=" + videoID);
+                        postBuilder.url("http://www.youtube.com/watch?v=" + videoID);
                     }
                 })
                 .map(YoutubeItem::getSnippet)
                 .subscribe(snippet -> {
                     try {
-                        post.setThumbnail(DrawableFetcher.getDrawableFromUrl(snippet.getThumbnails().getDefault().getUrl()));
-                        post.setTitle(snippet.getTitle());
-                        post.setDescription(snippet.getDescription());
+                        postBuilder.thumbnail(DrawableFetcher.getDrawableFromUrl(snippet.getThumbnails().getDefault().getUrl()));
+                        postBuilder.title(snippet.getTitle());
+                        postBuilder.description(snippet.getDescription());
                         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS", Locale.GERMANY);
-                        post.setDatetime(dateFormat.parse(snippet.getPublishedAt()));
-                        post.setPostType(VIDEO);
-                        posts.add(post);
+                        postBuilder.date(dateFormat.parse(snippet.getPublishedAt()));
+                        posts.add(postBuilder.build());
                     } catch (Exception e) {
                         PsLog.e(e.getMessage());
                         view.showError("YouTube parsing error");

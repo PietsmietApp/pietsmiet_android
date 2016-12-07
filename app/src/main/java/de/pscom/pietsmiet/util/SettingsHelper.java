ad6f96306c94e76fad142d5300f2e8cb344da4d3
Thomas Lu <thomasvanhalen01@gmail.com>
2016-12-07 17:15:56 +0100

    refactor xml files to avoid duplicate code

--- app/src/main/java/de/pscom/pietsmiet/util/SettingsHelper.java
+++ app/src/main/java/de/pscom/pietsmiet/util/SettingsHelper.java
@@ -5,13 +5,8 @@ import android.content.Context;
 import static de.pscom.pietsmiet.util.SharedPreferenceHelper.KEY_NEWS_SETTING;
 import static de.pscom.pietsmiet.util.SharedPreferenceHelper.getSharedPreferenceBoolean;
 
-/**
- * Created by Denny on 16.11.2016.
- */
-
 public class SettingsHelper {
     public static boolean uploadPlanNotification;
-    public static boolean andere_setting;
 
     public static void loadAllSettings(Context context){
         uploadPlanNotification = getSharedPreferenceBoolean(context, KEY_NEWS_SETTING,true);

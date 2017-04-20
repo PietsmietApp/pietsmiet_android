8e64c9d55c54c30084c602755cb3e1748fcdfd7f
Thomas Mey <substantialimpulse@protonmail.com>
2017-04-20 15:16:55 +0200

    add LeakCanary to detect memory leaks

--- app/src/main/java/de/pscom/pietsmiet/BaseActivity.java
+++ app/src/main/java/de/pscom/pietsmiet/BaseActivity.java
@@ -1,12 +1,23 @@
 package de.pscom.pietsmiet;
 
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
+import com.squareup.leakcanary.LeakCanary;
+
 public abstract class BaseActivity extends AppCompatActivity {
     protected Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (!LeakCanary.isInAnalyzerProcess(this)) {
+            LeakCanary.install(this.getApplication());
+        }
+    }
 
     protected void setupToolbar(@Nullable String title) {
         mToolbar = (Toolbar) findViewById(R.id.toolbar);

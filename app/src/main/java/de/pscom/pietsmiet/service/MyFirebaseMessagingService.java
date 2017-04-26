1873afaeb2fdd56d7083c44dd164584ed4f175a7
Thomas Mey <substantialimpulse@protonmail.com>
2017-04-26 20:29:58 +0200

    don't override old notifications
    2 videos on the same slot were not displayed

--- app/src/main/java/de/pscom/pietsmiet/service/MyFirebaseMessagingService.java
+++ app/src/main/java/de/pscom/pietsmiet/service/MyFirebaseMessagingService.java
@@ -17,7 +17,6 @@ package de.pscom.pietsmiet.service;
  */
 
 
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -29,28 +28,24 @@ import android.text.Html;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.util.Calendar;
 import java.util.Map;
 
 import de.pscom.pietsmiet.MainActivity;
 import de.pscom.pietsmiet.R;
-import de.pscom.pietsmiet.util.PostType;
 import de.pscom.pietsmiet.util.PsLog;
 
 import static android.app.PendingIntent.FLAG_ONE_SHOT;
 import static android.app.PendingIntent.FLAG_UPDATE_CURRENT;
 import static de.pscom.pietsmiet.util.PostType.NEWS;
 import static de.pscom.pietsmiet.util.PostType.PIETCAST;
-import static de.pscom.pietsmiet.util.PostType.UPLOADPLAN;
 import static de.pscom.pietsmiet.util.PostType.PS_VIDEO;
+import static de.pscom.pietsmiet.util.PostType.UPLOADPLAN;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     public static final String EXTRA_TYPE = "EXTRA_TYPE";
-    public static final String EXTRA_NOTIFICATION_ID = "de.pscom.pietsmiet.EXTRA_NOTIFICATION_ID";
     public static final String KEY_UNSUBSCRIBE = "de.pscom.pietsmiet.KEY_UNSUBSCRIBE";
-    @PostType.AllTypes
-    private int postType;
-    private int notificationId;
 
     /**
      * Called when message is received.
@@ -59,53 +54,47 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
      */
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         PsLog.d("From: " + remoteMessage.getFrom());
         // Check if message contains a data payload.
         Map<String, String> data = remoteMessage.getData();
-        if (data.size() > 0) {
-            PsLog.d("Message data payload: " + data);
-            int type;
-            switch (data.get("topic")) {
-                case "news":
-                    type = NEWS;
-                    notificationId = NEWS;
-                    PsLog.v("hi");
-                    break;
-                case "uploadplan":
-                    type = UPLOADPLAN;
-                    notificationId = UPLOADPLAN;
-                    break;
-                case "pietcast":
-                    type = PIETCAST;
-                    notificationId = PIETCAST;
-                    break;
-                case "video":
-                    type = PS_VIDEO;
-                    notificationId = PS_VIDEO;
-                    break;
-                default:
-                    PsLog.w("Falsche Kategorie " + data.get("topic"));
-                    return;
-            }
-            // On notification click intent
-            Intent clickIntent = new Intent(this, MainActivity.class);
-            clickIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            clickIntent.putExtra(EXTRA_TYPE, type);
-            PendingIntent clickPIntent = PendingIntent.getActivity(this, notificationId, clickIntent,
-                    FLAG_ONE_SHOT | FLAG_UPDATE_CURRENT);
-
-            // On action button click intent
-            Intent urlIntent = new Intent(Intent.ACTION_VIEW);
-            urlIntent.setData(Uri.parse(data.get("link")));
-            PendingIntent urlPIntent = PendingIntent.getActivity(this, notificationId, urlIntent,
-                    FLAG_ONE_SHOT | FLAG_UPDATE_CURRENT);
-
-            sendNotification(data.get("title"), data.get("message"), clickPIntent, urlPIntent, type);
+        if (data.size() == 0) {
+            PsLog.w("No data provided");
+            return;
         }
+        PsLog.d("Message data payload: " + data);
+        int type;
+        switch (data.get("topic")) {
+            case "news":
+                type = NEWS;
+                PsLog.v("hi");
+                break;
+            case "uploadplan":
+                type = UPLOADPLAN;
+                break;
+            case "pietcast":
+                type = PIETCAST;
+                break;
+            case "video":
+                type = PS_VIDEO;
+                break;
+            default:
+                PsLog.w("Falsche Kategorie " + data.get("topic"));
+                return;
+        }
+        // On notification click intent
+        Intent clickIntent = new Intent(this, MainActivity.class);
+        clickIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        clickIntent.putExtra(EXTRA_TYPE, type);
+        PendingIntent clickPIntent = PendingIntent.getActivity(this, type, clickIntent,
+                FLAG_ONE_SHOT | FLAG_UPDATE_CURRENT);
 
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
+        // On action button click intent
+        Intent urlIntent = new Intent(Intent.ACTION_VIEW);
+        urlIntent.setData(Uri.parse(data.get("link")));
+        PendingIntent urlPIntent = PendingIntent.getActivity(this, type, urlIntent,
+                FLAG_ONE_SHOT | FLAG_UPDATE_CURRENT);
+
+        sendNotification(data.get("title"), data.get("message"), clickPIntent, urlPIntent, type);
     }
 
     /**
@@ -121,21 +110,29 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 FLAG_ONE_SHOT | FLAG_UPDATE_CURRENT);
 
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.mipmap.ic_launcher)
+                .setSmallIcon(R.mipmap.ic_launcher) //fixme one day: a different notif icon
                 .setContentTitle(title)
-                .addAction(R.drawable.ic_remove_black_24dp, "Abbestellen", unsubscribePIntent)
-                .setAutoCancel(true)
-                .setWhen(0)
-                .setPriority(Notification.PRIORITY_MAX);
+                .addAction(R.drawable.ic_remove_black_24dp, getString(R.string.notification_unsubscribe), unsubscribePIntent)
+                .setGroup(type + "")
+                .setAutoCancel(true);
         if (type == PS_VIDEO) {
             notificationBuilder.setContentIntent(urlIntent);
         } else {
             notificationBuilder.setContentIntent(clickIntent);
-            notificationBuilder.addAction(R.drawable.ic_open_in_browser_black_24dp, "Link öffnen", urlIntent);
+            notificationBuilder.addAction(R.drawable.ic_open_in_browser_black_24dp, getString(R.string.notification_open_url), urlIntent);
         }
+        
         if (messageBody != null) {
             notificationBuilder.setStyle(new NotificationCompat.BigTextStyle().bigText(Html.fromHtml(messageBody)));
             notificationBuilder.setContentText(Html.fromHtml(messageBody));
+        }
+
+        int notificationId;
+        if (type == UPLOADPLAN) {
+            // Override existing notifications by providing same Id
+            notificationId = type;
+        } else {
+            notificationId = Calendar.getInstance().get(Calendar.SECOND);
         }
 
         NotificationManager notificationManager =
